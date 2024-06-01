e73eca478cf98f10c087888a21ed764d0f3aea51
Sudhanshu selokar <91870881+sudhanshu-selokar@users.noreply.github.com>
2024-06-01 16:25:30 +0530

    Update Commom-Element-Matrix.js

--- DSA Assignments/Day 4/Commom-Element-Matrix.js
+++ DSA Assignments/Day 4/Commom-Element-Matrix.js
@@ -3,15 +3,10 @@ function findCommonElements(matrix) {
 
     let rows = matrix.length;
     let cols = matrix[0].length;
-
     let hashMap = {};
-    
-    // Initialize the hash map with the first row
     for (let j = 0; j < cols; j++) {
         hashMap[matrix[0][j]] = 1;
     }
-
-    // Process each row starting from the second row
     for (let i = 1; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             if (hashMap[matrix[i][j]] === i) {
@@ -20,7 +15,6 @@ function findCommonElements(matrix) {
         }
     }
 
-    // Collect all elements that are present in all rows
     let result = [];
     for (let key in hashMap) {
         if (hashMap[key] === rows) {
